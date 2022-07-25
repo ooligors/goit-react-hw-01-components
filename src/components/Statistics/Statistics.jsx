f7934862137f5e8f4880fbb9436dfa555ec85178
ooligors <ooligors@gmail.com>
2022-07-25 19:43:35 +0300

    gnfgn

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { PropTypes } from "prop-types";
-import randomColor from "randomcolor";
+// import randomColor from "randomcolor";
 import css from './Statistics.module.css';
 export const Statistics = ({ title, stats }) => {
   return (
@@ -11,7 +11,8 @@ export const Statistics = ({ title, stats }) => {
         {stats.map((stat) => {
           console.log(stat);
           return (
-          <li className={css.item} key={stat.id} style={{backgroundColor:randomColor() }} >
+            // style={{backgroundColor:randomColor() }}
+          <li className={css.item} key={stat.id}  >
             <span className={css.label}>{stat.label}</span>
             <span className={css.percentage}>{stat.percentage}%</span>
           </li>
