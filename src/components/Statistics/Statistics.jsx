85917eb48b0a0dcb45a6afeec46721479bd46117
ooligors <ooligors@gmail.com>
2022-07-25 19:48:53 +0300

    fff

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,27 +1,31 @@
 import React from 'react';
-import { PropTypes } from "prop-types";
-// import randomColor from "randomcolor";
+import { PropTypes } from 'prop-types';
+import randomColor from 'randomcolor';
 import css from './Statistics.module.css';
 export const Statistics = ({ title, stats }) => {
   return (
     <section className={css.statistics}>
       {title && <h2 className={css.title}>{title}</h2>}
-     
+
       <ul className={css.statList}>
-        {stats.map((stat) => {
+        {stats.map(stat => {
           console.log(stat);
           return (
-            // style={{backgroundColor:randomColor() }}
-          <li className={css.item} key={stat.id}  >
-            <span className={css.label}>{stat.label}</span>
-            <span className={css.percentage}>{stat.percentage}%</span>
-          </li>
-        )})}
+            <li
+              className={css.item}
+              key={stat.id}
+              style={{ backgroundColor: randomColor() }}
+            >
+              <span className={css.label}>{stat.label}</span>
+              <span className={css.percentage}>{stat.percentage}%</span>
+            </li>
+          );
+        })}
       </ul>
     </section>
   );
 };
 Statistics.propTypes = {
-    title: PropTypes.string,
-    stats: PropTypes.arrayOf(PropTypes.object).isRequired,
-}
+  title: PropTypes.string,
+  stats: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
